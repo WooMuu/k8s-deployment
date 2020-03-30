a8279e45e7dc0d5ee4ac16a6f8f053b1a8151379
张建斌 <zhangjianbin@qq.com>
2020-03-30 17:24:25 +0800

    取消备份

--- kubeadm_v1.17.4/use_aliyun_yum_source.sh
+++ kubeadm_v1.17.4/use_aliyun_yum_source.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 yum install wget -y
-#1、备份
-DN= '/etc/yum.repos.d/CentOS-Base.repo'
-if [-d $DN];
-   then mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
-else
-   echo $DN 'not exist'
-fi
 
 cp -p ./CentOS7-Aliyun.repo /etc/yum.repos.d
 cp -p ./epel-7-Aliyun.repo /etc/yum.repos.d
