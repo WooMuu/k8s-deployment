32cf4270e2387996f366759c25a81aaf8b331462
张建斌 <zhangjianbin@qq.com>
2020-03-30 19:38:31 +0800

    不使用setenforce 0

--- kubeadm_v1.17.4/03_install_kubernetes.sh
+++ kubeadm_v1.17.4/03_install_kubernetes.sh
@@ -4,7 +4,7 @@ set -e
 
 ./use_aliyun_kubernetes_yum_source.sh
 
-setenforce 0
+# setenforce 0
 # Use Kubernetes-cni-0.6.0 explictly
 # https://github.com/kubernetes/kubernetes/issues/75701
 yum install -y kubelet-1.17.4 kubeadm-1.17.4 kubectl-1.17.4 --disableexcludes=kubernetes
