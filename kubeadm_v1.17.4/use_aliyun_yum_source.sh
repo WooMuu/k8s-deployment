b6cc45ab0df2cd8478f90006a036792edec52bdc
张建斌 <zhangjianbin@qq.com>
2020-03-30 17:20:06 +0800

    判断mv /etc/yum.repos.d/CentOS-Base.repo 是否存在

--- kubeadm_v1.17.4/use_aliyun_yum_source.sh
+++ kubeadm_v1.17.4/use_aliyun_yum_source.sh
@@ -2,7 +2,13 @@
 set -e
 yum install wget -y
 #1、备份
-mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+DN= '/etc/yum.repos.d/CentOS-Base.repo'
+if [-d $DN];
+   then mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+else
+   echo $DN 'not exist'
+fi
+
 cp -p ./CentOS7-Aliyun.repo /etc/yum.repos.d
 cp -p ./epel-7-Aliyun.repo /etc/yum.repos.d
 
