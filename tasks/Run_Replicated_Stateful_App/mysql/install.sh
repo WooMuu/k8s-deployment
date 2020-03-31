36fee7984837e6f9f422fbcce66940b2695db25b
张建斌 <zhangjianbin@qq.com>
2020-03-31 19:35:40 +0800

    pv

--- tasks/Run_Replicated_Stateful_App/mysql/install.sh
+++ tasks/Run_Replicated_Stateful_App/mysql/install.sh
@@ -2,6 +2,8 @@
 set -e
 kubectl apply -f ./mysql-configmap.yaml
 kubectl apply -f ./mysql-services.yaml
+
+kubectl apply -f ./mysql-storageClass.yaml
 #创建pv
 kubectl apply -f ./mysql-pv.yaml
 kubectl apply -f ./mysql-statefulset.yaml
