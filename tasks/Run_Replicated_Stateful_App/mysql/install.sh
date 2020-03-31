1fba049f741801ea078aa81552c7cdcb8f28154a
张建斌 <zhangjianbin@qq.com>
2020-03-31 15:02:13 +0800

    修改pv配置文件

--- tasks/Run_Replicated_Stateful_App/mysql/install.sh
+++ tasks/Run_Replicated_Stateful_App/mysql/install.sh
@@ -3,5 +3,5 @@ set -e
 kubectl apply -f ./mysql-configmap.yaml
 kubectl apply -f ./mysql-services.yaml
 #创建pv
-kubectl apply -f ./mysql-pv1.yaml
+kubectl apply -f ./mysql-pv.yaml
 kubectl apply -f ./mysql-statefulset.yaml
