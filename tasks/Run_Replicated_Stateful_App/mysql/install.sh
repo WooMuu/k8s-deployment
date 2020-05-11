53383400b960b270791d0eab0e8b93a2f352f728
张建斌 <zhangjianbin@qq.com>
2020-05-11 23:37:31 +0800

    delete

--- tasks/Run_Replicated_Stateful_App/mysql/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-kubectl apply -f ./mysql-configmap.yaml
-kubectl apply -f ./mysql-services.yaml
-
-kubectl apply -f ./mysql-storageClass.yaml
-#创建pv
-kubectl apply -f ./mysql-pv.yaml
-kubectl apply -f ./mysql-statefulset.yaml
