6656823e46aed6ae64010d04ad5f0f967010aac2
张建斌 <zhangjianbin@qq.com>
2021-01-04 20:41:28 +0800

    pull ingress

--- k8s_tasks/access_application_in_a_cluster/ingress/deploy/04_pull_ingress_controller_from_aliyun.sh
+++ k8s_tasks/access_application_in_a_cluster/ingress/deploy/04_pull_ingress_controller_from_aliyun.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 set -e
-docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/nginx-ingress-controller:0.20.0
-docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/nginx-ingress-controller:0.20.0 quay.io/kubernetes-ingress-controller/nginx-ingress-controller:0.20.0
-docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/nginx-ingress-controller:0.20.0
+docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/nginx-ingress-controller:0.29.0
+docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/nginx-ingress-controller:0.29.0 quay.io/kubernetes-ingress-controller/nginx-ingress-controller:0.29.0
+docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/nginx-ingress-controller:0.29.0
 
 docker pull registry.cn-hangzhou.aliyuncs.com/yuheng/defaultbackend-amd64:1.5
 docker tag registry.cn-hangzhou.aliyuncs.com/yuheng/defaultbackend-amd64:1.5  k8s.gcr.io/defaultbackend-amd64:1.5
 docker rmi registry.cn-hangzhou.aliyuncs.com/yuheng/defaultbackend-amd64:1.5
-
-docker pull
-docker tag
-docker rmi
 
 docker images
 
